860c988db1d1c4b519bfd1b8e72e6f146ad032c3
VinDicky <kevindck1610@gmail.com>
2025-01-03 22:37:39 +0700

    AI streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import torch
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
 from model import WaterCapacityModel
 
